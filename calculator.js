5725951189fe3362abb4d38388d25ce29d7b8ef5
mdlufy <german.panov.2018@mail.ru>
2022-08-01 16:33:57 +0300

    feature: add roman calc

--- calculator.js
+++ calculator.js
@@ -15,13 +15,17 @@ function calculator(string) {
         'X': 10
     }
 
+    let flag = 0;
+
     function toArabic(value) {
         let newValue;
 
         if (value in arabic) {
             newValue = arabic[value];
+            flag++;
         } else {
             newValue = Number(value);
+            if (newValue === 0) throw Error;
         }
 
         if (typeof newValue !== 'number') {
@@ -31,26 +35,40 @@ function calculator(string) {
         }
     }
 
-    const newFirst = toArabic(first);
-    const newSecond = toArabic(second);
-
-    console.log(newFirst);
-    console.log(newSecond);
-
+    const newArabicFirst = toArabic(first);
+    const newArabicSecond = toArabic(second);
     
-    var toOperator = {
+    const toOperator = {
         '+': function (x, y) { return x + y },
         '-': function (x, y) { return x - y },
         '*': function (x, y) { return x * y },
         '/': function (x, y) { return Math.trunc(x / y) }
     }
 
-    const result = toOperator[operation](newFirst, newSecond);
+    function toRoman(value) {
+        const romans = [
+              ['',"I","II","III","IV","V","VI","VII","VIII","IX"],
+              ['',"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"],
+              ['',"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
+              ['',"M","MM","MMM"]
+              ];
+        const romanValue = romans[3][Math.floor(value / 1000 % 10)] + romans[2][Math.floor(value / 100 % 10)] + romans[1][Math.floor(value / 10 % 10)] + romans[0][Math.floor(value % 10)];
+
+        return romanValue;
+    }
+        
+    let result = toOperator[operation](newArabicFirst, newArabicSecond);
+
+
+    if (flag === 1) throw Error;
+    if (flag === 2 && result > 0) {
+        result = toRoman(result);
+    }
+    if (flag === 2 && result <= 0) {
+        return '';
+    }
 
     return result.toString();
-
-
-
 }
 
 
