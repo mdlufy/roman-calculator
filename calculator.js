b0760b72738ff14d159970d5bb7018453e17c682
mdlufy <german.panov.2018@mail.ru>
2022-08-01 15:21:41 +0300

    feature: work with arabic

--- calculator.js
+++ calculator.js
@@ -1,5 +1,57 @@
 function calculator(string) {
 
+    const [first, operation, second] = string.split(" ");
+
+    const arabic = {
+        'I': 1,
+        'II': 2,
+        'III': 3,
+        'IV': 4,
+        'V': 5,
+        'VI': 6,
+        'VII': 7,
+        'VIII': 8,
+        'IX': 9,
+        'X': 10
+    }
+
+    function toArabic(value) {
+        let newValue;
+
+        if (value in arabic) {
+            newValue = arabic[value];
+        } else {
+            newValue = Number(value);
+        }
+
+        if (typeof newValue !== 'number') {
+            throw Error;
+        } else {
+            return newValue
+        }
+    }
+
+    const newFirst = toArabic(first);
+    const newSecond = toArabic(second);
+
+    console.log(newFirst);
+    console.log(newSecond);
+
+    
+    var toOperator = {
+        '+': function (x, y) { return x + y },
+        '-': function (x, y) { return x - y },
+        '*': function (x, y) { return x * y },
+        '/': function (x, y) { return Math.trunc(x / y) }
+    }
+
+    const result = toOperator[operation](newFirst, newSecond);
+
+    return result.toString();
+
+
+
 }
+
 
 module.exports = calculator;
